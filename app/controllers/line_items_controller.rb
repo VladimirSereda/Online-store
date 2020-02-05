94dc3413bb14c9ac4ad188f3398e22d2ee3a5823
Vladimir <vovkati3@gmail.com>
2020-02-05 19:27:34 +0300

    finish A13

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -22,6 +22,7 @@ class LineItemsController < ApplicationController
   # GET /line_items/1/edit
   def edit
   end
+  
 
   # POST /line_items
   # POST /line_items.json
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
-  resources :line_items
-  resources :carts
-  get 'store/index'
-  resources :products
+resources :line_items
+resources :carts
+get 'store/index'
+resources :products
 
-  root to: 'store#index', as: 'store'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+root to: 'store#index', as: 'store'
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
